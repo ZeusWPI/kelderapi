6f69f1f0afcfc6c9988473a03b3c5fe36cd6e842
draxaris1010 <axel.dezittere@gmail.com>
2024-11-29 16:01:33 +0100

    updates

--- screen.sh
+++ screen.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 export DISPLAY=${DISPLAY:-:$(ls -v /tmp/.X11-unix/ | head -n 1 | cut -dX -f2)}
 if [ x"$1" = x"on" ]; then
-<<<<<<< HEAD
     xset dpms force on
     xset s off
     xset s noblank
