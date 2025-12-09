fc0834ead0ccab9af56897d8227dd5fbb3bf0bc7
Jonas Meeuws <jonas.meeuws@gmail.com>
2025-12-10 00:41:58 +0100

    fix(mqtt2other): Fix match-case, trigger screen.sh properly.

--- scripts/mqtt2other.py
+++ scripts/mqtt2other.py
@@ -13,6 +13,14 @@ topics = [
 ]
 
 
+def screen_on():
+    print("running \"screen.sh on\"")
+    subprocess.Popen(["/opt/kelderapi/screen.sh", "on"])
+
+def screen_off():
+    print("running \"screen.sh off\"")
+    subprocess.Popen(["/opt/kelderapi/screen.sh", "off"])
+
 def on_message(client, userdata, msg):
     print(f'Received message on topic "{msg.topic}"')
     try:
@@ -26,7 +34,7 @@ def on_message(client, userdata, msg):
             data = f"ScrollingText Welkom {username}!"
             print(f"sending {data} to leddy")
             requests.post("http://leddy.kelder.local/", data)
-        case "zigbee2mqtt/all" "zigbee2mqtt/group_switch_devices" "zigbee2mqtt/devices":
+        case "zigbee2mqtt/all" | "zigbee2mqtt/devices" | "zigbee2mqtt/group_switch_devices":
             action = payload_dict.get("action")
             state = payload_dict.get("state")
             if action == "on" or state == "ON":
@@ -41,9 +49,12 @@ def on_message(client, userdata, msg):
 
 client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 client.on_message = on_message
-client.connect(broker_host, broker_port, 60)
+
+print(f"Connecting to {broker_host} {broker_port}")
+client.connect(broker_host, broker_port, 1)
 
 for topic in topics:
+    print(f"Subscribing to {topic}")
     client.subscribe(topic)
 
 client.loop_forever()
