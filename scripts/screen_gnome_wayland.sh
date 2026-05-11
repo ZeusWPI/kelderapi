1263f351e0f1ed38a7b2d7f088ad260c967d3895
draxaris1010 <axel.dezittere@gmail.com>
2026-05-11 13:55:08 +0200

    anti earrape

--- scripts/screen_gnome_wayland.sh
+++ scripts/screen_gnome_wayland.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 # zie https://gitlab.gnome.org/GNOME/mutter/-/blob/main/data/dbus-interfaces/org.gnome.Mutter.DisplayConfig.xml#L287
 if [ x"$1" = x"on" ]; then
+    /usr/sbin/rfkill unblock bluetooth
     gdbus call --session \
         --dest org.gnome.Mutter.DisplayConfig \
         --object-path /org/gnome/Mutter/DisplayConfig \
         --method org.freedesktop.DBus.Properties.Set \
         org.gnome.Mutter.DisplayConfig PowerSaveMode '<@i 0>'
+    pactl set-sink-volume @DEFAULT_SINK@ 20%
 elif [ x"$1" = x"off" ]; then
+    /usr/sbin/rfkill block bluetooth
     gdbus call --session \
         --dest org.gnome.Mutter.DisplayConfig \
         --object-path /org/gnome/Mutter/DisplayConfig \
